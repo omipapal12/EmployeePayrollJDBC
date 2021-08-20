4c0f6f0b084f5c3a1ca3176599bcf4e8bdce17d7
omipapal12 <omipapal12@gmail.com>
2021-08-20 09:20:12 +0530

    Added: Test For Payroll Serive Database

---
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    void givenDataInDB_WhenRetrieved_ShouldMatchTheCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayroll> employeePayrolls = employeePayrollService.readEmployeePayrollData();
+        Assertions.assertEquals(3,  employeePayrolls.size());
+    }
+}
+    
